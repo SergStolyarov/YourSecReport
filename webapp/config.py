73f6d89553a89802287a6d609b30d09817e3a669
Evgeniy <evgeniysgs3@gmail.com>
2018-02-26 12:15:27 +0300

    Изменил config

--- webapp/config.py
+++ webapp/config.py
@@ -6,11 +6,10 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(24)
-    MONGODB_HOST = '212.109.192.124'
-    MONGODB_DBNAME = 'test_db'
-    MONGODB_USERNAME = 'admin'
-    MONGODB_PASSWORD = 'yoursecreport'
-    
+    MONGODB_HOST = '*********'
+    MONGODB_DBNAME = '********'
+    MONGODB_USERNAME = '********'
+    MONGODB_PASSWORD = '**********'
 
 class ProductionConfig(Config):
     DEBUG = False
