f580da68e1ef399fb37c4aed62340fec333218de
chud0 <chud0@mail.ru>
2018-03-02 10:55:15 +0300

    update methods, add check_report_time

--- data_base/bd_example.py
+++ data_base/bd_example.py
@@ -1,6 +1,6 @@
-from bd_app import *
+from . import bd_app
 
-client = Client_DB()
+client = bd_app.Client_DB()
 
 """Клиенты. Еесли клиента с таким логином нет - запишет и вернет True, иначе False. дата - время записи"""
 print(client.add_client("test_client", "hash_pass", "example@email.com", "555.555.555.555"))
@@ -9,7 +9,9 @@ print(client.add_client("test_client", "hash_pass", "example@email.com", "555.55
 print(client.get_client("test_client"))
 
 print("*" * 10)
-"""можно отдельно получать поля соответствующими методами. target (ip для скана) можно редактировать"""
+"""можно отдельно получать поля соответствующими методами.
+target (ip для скана) можно редактировать, не может быть больше 5 штук"""
+
 print(client.add_target("test_client", "555.555.555.556"))
 print(client.get_target("test_client"))
 print(client.del_target("test_client", "555.555.555.556"))
@@ -17,17 +19,17 @@ print(client.get_target("test_client"))
 print(client.del_target("test_client", "555.555.555.556"))
 
 
-report = Report_DB()
+report = bd_app.Report_DB()
 print("*" * 10)
 
 """Отчеты. можно записать, изменять потом нельзя"""
-print(report.add_report("test_client", "555.555.555.555", "test_report"))
-report.add_report("test_client", "555.555.555.555", "test_report2")
-report.add_report("test_client", "555.555.555.556", "test_report")
+print(report.add_report("test_client", "555.555.555.556", "nmap", "test_report_nmap"))
+report.add_report("test_client", "555.555.555.556", "nikto", "test_report_nikto")
 
-"""есть фильтры по лошину и ip"""
-print(report.get_report(login="test_client"))
-print(report.get_report(login="test_client", target="555.555.555.556"))
+"""есть фильтры по логину, ip и типу отчета"""
+# print(report.get_report(login="test_client"))
+# print(report.get_report(login="test_client", target="555.555.555.555"))
+print(report.get_report(login="test_client", target="555.555.555.555", report_type="nmap"))
 
 """можно получить последний отчет, тоже есть фильтры"""
-print(report.get_last_report(login="test_client", target="555.555.555.555"))
+print(report.get_last_report(login="test_client", target="555.555.555.556", report_type="nikto"))
